25cba211909f0bbe85f867490c2104ceb3850eb5
DESKTOP-2DOQUL1\경수 <96224@naver.com>
2022-03-31 19:26:04 +0900

    inlement game loop

---
+++ Socoban/Socoban/Framework/Game.c
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "Game.h"
+
+bool Initialize()
+{
+	return true;
+}
+
+void processInput()
+{
+
+}
+
+void update()
+{
+
+}
+
+void render()
+{
+
+}
+
+int32_t Run()
+{
+	// Game Loop 모든 게임에 있다
+	// 플레이어의 입력을 하드웨어랑 분리시킨다.
+	// https://gameprogrammingpatterns.com/game-loop.html
+
+	// Game Loop의 전체를 Frame이라고 부른다.
+	while (true)
+	{
+		// 입력 처리
+		processInput();
+		// 업데이트
+		update();
+		// 렌더링
+		render(); 
+	}
+}
--- Socoban/Socoban/main.c
+++ Socoban/Socoban/main.c
@@ -1,6 +1,14 @@
 #include "stdafx.h"
+#include "Framework/Game.h"
 
 int main()
 {
-	puts("경수 화이팅이야");
+	if (false == Initialize())
+	{
+		puts("게임을 로딩하는 데 문제가 생겼습니다.");
+
+		return 1;
+	}
+
+	return Run();
 }
